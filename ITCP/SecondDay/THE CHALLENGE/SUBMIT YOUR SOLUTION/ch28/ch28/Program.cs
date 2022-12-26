52d9f814cb37c11e93615e053b61649c7ccca834
Mohamed-Rebbouh <121132111+Mohamed-Rebbouh@users.noreply.github.com>
2022-12-26 13:39:35 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/ch28/ch28/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class ch28
+{
+    static void Main(string[] args)
+    {
+        int n, i,a,b;
+        n=Convert.ToInt32(Console.ReadLine());
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+         
+        for (i = 0; i < n; i++)
+        {
+            if (a % i == 0)
+                Console.WriteLine("it");
+            else if (b % i == 0) 
+                Console.WriteLine("ccc");
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
