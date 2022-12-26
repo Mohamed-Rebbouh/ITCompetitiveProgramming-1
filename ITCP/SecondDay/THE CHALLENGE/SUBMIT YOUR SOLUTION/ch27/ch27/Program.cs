2eb633f1da1a8cf443b0cf5bc4104dbf73f6ddd7
Mohamed-Rebbouh <121132111+Mohamed-Rebbouh@users.noreply.github.com>
2022-12-26 13:39:31 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/ch27/ch27/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Schema;
+
+class ch20
+{
+    static void Main(string[] args)
+    {
+        int x, y;
+        x=Convert.ToInt32(Console.ReadLine());
+        y= Convert.ToInt32(Console.ReadLine());
+
+        if (x + y < 100)
+            Console.WriteLine("true");
+        else
+            Console.WriteLine("false");
+
+
+
+
+    }
+
+}
