3134dd229921379cce9203c3dc2de787e49e1c56
Mohamed-Rebbouh <121132111+Mohamed-Rebbouh@users.noreply.github.com>
2022-12-26 13:29:24 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/ch9/ch9/Program.cs
@@ -0,0 +1,52 @@
+﻿class ch9
+{
+    public static int tab1(int[] tab, int[] t1, int[] t2, int s1, int s2)
+    {
+
+
+        int i, j, cpt = 0;
+        for (i = 0; i < s1; i++)
+        {
+
+            for (j = 0; j < s2; j++)
+            {
+                if (t1[i] == t2[j])
+                {
+                    tab[cpt] = t1[i];
+                    cpt++;
+
+                }
+
+
+
+
+            }
+
+
+
+
+        }
+        return cpt;
+    }
+    static void Main()
+    {
+        int s, i;
+        int[] a = { 1, 2, 3, 4 };
+        int[] b = { 1, 2, 5, 67 };
+        int[] c = new int[50];
+
+        s = tab1(c, a, b, 4, 4);
+        for (i = 0; i < s; i++)
+        {
+            Console.Write(Convert.ToString(c[i]));
+            Console.Write("-");
+
+        }
+
+
+    }
+
+
+
+
+}
