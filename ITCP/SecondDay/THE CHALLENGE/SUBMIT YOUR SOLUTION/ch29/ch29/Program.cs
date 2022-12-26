194391b284eeea6de79546ccabc87a9f72c804bb
Mohamed-Rebbouh <121132111+Mohamed-Rebbouh@users.noreply.github.com>
2022-12-26 13:39:40 +0100

    Add files via upload

---
+++ ITCP/SecondDay/THE CHALLENGE/SUBMIT YOUR SOLUTION/ch29/ch29/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class ch29
+{
+    static int somme (int x, int y)
+    {
+
+
+        return x + y;
+
+    }
+
+    static void Main(string[] args)
+    {
+
+        int x, y;
+        Console.WriteLine("doner number:");
+        x=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("doner number:");
+        y= Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("sommer is="+Convert.ToString(somme(x, y)));
+
+
+
+    }
+
+
+
+
+}
